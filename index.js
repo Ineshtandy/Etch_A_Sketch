38a5d048d50376f766986b42421071b68be005d2
Inesh <i08polymath@gmail.com>
2022-07-18 00:44:47 +0530

    JAVASCRIPT UPDATE
    added and commented attempt to add hover style using
        event listener
    the function is adding bg colour but that is constant, need to remove it too

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ for(let i =0;i<16;i++){
     sqdiv.setAttribute("class","colel");
     sqdiv.textContent = i+" "+j;
     rowdiv.appendChild(sqdiv);
-    //console.log(sqdiv);
+
+    /*sqdiv.addEventListener("mouseover",hover);*/
+    
 
   }
   container.appendChild(rowdiv);
 }
+
+//this is constantly adding colour
+/*function hover(){
+  this.setAttribute("style","background-color:green;");
+}*/
